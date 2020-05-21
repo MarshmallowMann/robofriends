4fc24536db470255a652abd49eae5012b6c9c0b5
MarshmallowMann <johnrobertson.despi@gmail.com>
2020-05-22 05:04:49 +0800

    Updates

---
+++ precache-manifest.7e8a19d410a5da11e55a169f261f0d02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d622b2fbc38bc9483b8c9467680be3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1493ebbe320ed2394077",
+    "url": "/robofriends/static/css/main.3449bf88.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1493ebbe320ed2394077",
+    "url": "/robofriends/static/js/main.c4f24e55.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "e6108fee300bcc7274806cec4c44f52d",
+    "url": "/robofriends/static/media/HACKED_Title.e6108fee.woff"
+  }
+]);
